a3a539d472f77b77c9ae5fe166a3e611ffcb4153
Shakil Ahmad <shakiluahmad@gmail.com>
2026-02-15 17:27:29 +0000

    File changes

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -277,7 +277,7 @@ export default function Calculator() {
                 {isNewProject ? 'Create a new tape light quote' : 'Edit project details and runs'}
               </p>
             </div>
-            <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-2 w-full">
               {!isNewProject && (
                 <div className="flex gap-2 w-full">
                   <Button variant="outline" size="sm" onClick={handleExportQuote} className="flex-1 h-8 justify-center text-xs">
