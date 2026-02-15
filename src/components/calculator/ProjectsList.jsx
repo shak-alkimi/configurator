5f25d3520054c787661dd19c81f47743a9c88a33
Shakil Ahmad <shakiluahmad@gmail.com>
2026-02-15 21:50:41 +0000

    File changes

--- src/components/calculator/ProjectsList.jsx
+++ src/components/calculator/ProjectsList.jsx
@@ -37,7 +37,7 @@ export default function ProjectsList({ projects, selectedId, onSelect, onNew, is
         <div className="relative">
           <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-slate-400" />
           <Input
-            placeholder="Search projects..."
+            placeholder=""
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
             className="pl-9 h-9"
