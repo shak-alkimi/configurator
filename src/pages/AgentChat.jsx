a294eb134272cc31ebe71932f64e61e00de33345
Shakil Ahmad <shakiluahmad@gmail.com>
2026-02-15 21:41:22 +0000

    File changes

--- src/pages/AgentChat.jsx
+++ src/pages/AgentChat.jsx
@@ -64,7 +64,7 @@ export default function AgentChat() {
     <div className="h-screen flex flex-col bg-slate-50">
       {/* Header */}
       <div className="border-b border-slate-200 bg-white">
-        <div className="flex items-center py-6 pr-6 pl-0">
+        <div className="flex items-center p-6">
           <img 
             src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/698fc81203f85a20f281d9dc/f2bc037c5_Screenshot2026-02-14160229.png" 
             alt="ALKIMI Logo"
