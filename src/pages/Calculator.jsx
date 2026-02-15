d3c359e9c21e0f8ec8caa1d069deff5032e6da64
Shakil Ahmad <shakiluahmad@gmail.com>
2026-02-15 21:19:09 +0000

    Tape Light Quote Creation

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -288,11 +288,11 @@ export default function Calculator() {
       {/* Sidebar - Projects List */}
       <div className="w-80 border-r bg-white">
         <div className="h-full flex flex-col">
-          <div className="pt-4 pb-4 border-b">
+          <div className="pt-4 pr-4 pb-4 border-b">
             <img 
               src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/698fc81203f85a20f281d9dc/f2bc037c5_Screenshot2026-02-14160229.png" 
               alt="ALKIMI Logo"
-              className="h-12 mb-3 w-32"
+              className="h-12 mb-3 w-full object-cover object-left"
               style={{ filter: 'invert(1)' }}
             />
             <p className="text-xs text-slate-500 pl-4">Project Quotes</p>
