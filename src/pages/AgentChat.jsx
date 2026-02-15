3ba729b97e5e7fcf3684a4bd43f28e8e44c3939a
Shakil Ahmad <shakiluahmad@gmail.com>
2026-02-15 23:52:53 +0000

    File changes

--- src/pages/AgentChat.jsx
+++ src/pages/AgentChat.jsx
@@ -75,7 +75,7 @@ export default function AgentChat() {
       </div>
 
       {/* Messages */}
-      <div className="flex-1 overflow-y-auto p-4 sm:p-6 space-y-4">
+      <div className="flex-1 overflow-y-auto p-3 sm:p-6 space-y-3 min-h-0">
         {messages.length === 0 ? (
           <div className="h-full flex items-center justify-center">
             <div className="text-center">
