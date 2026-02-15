4805f6b0e8f2dcec37ce01806cd742321e42a550
Shakil Ahmad <shakiluahmad@gmail.com>
2026-02-15 18:32:01 +0000

    File changes

--- src/components/calculator/TapeRunList.jsx
+++ src/components/calculator/TapeRunList.jsx
@@ -144,7 +144,7 @@ export default function TapeRunList({ runs, onAdd, onUpdate, onDelete }) {
                 className="h-9"
               />
             </div>
-            <div className="col-span-2 space-y-1.5">
+            <div className="col-span-2 space-y-2">
               <Label className="text-xs">Color Temperature</Label>
               <Select
                 value={newRun.tape_type}
