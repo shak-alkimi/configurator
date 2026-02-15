2ba11fa09f1290bc8dc2e2ff82a55a3742e31fc9
Shakil Ahmad <shakiluahmad@gmail.com>
2026-02-15 18:32:54 +0000

    File changes

--- src/components/calculator/TapeRunList.jsx
+++ src/components/calculator/TapeRunList.jsx
@@ -195,7 +195,7 @@ export default function TapeRunList({ runs, onAdd, onUpdate, onDelete }) {
                 </SelectContent>
               </Select>
             </div>
-            <div className="col-span-1 space-y-1.5">
+            <div className="col-span-1 space-y-2">
               <Label className="text-xs">Output</Label>
               <Select
                 value={newRun.output}
