66c4c2488652f81b1a6d42e2cd7d2c0be735a38b
Shakil Ahmad <shakiluahmad@gmail.com>
2026-02-16 13:41:20 +0000

    File changes

--- src/pages/AgentChat.jsx
+++ src/pages/AgentChat.jsx
@@ -61,8 +61,8 @@ export default function AgentChat() {
   };
 
   return (
-    <div className="flex-1 overflow-y-auto hide-scrollbar flex flex-col bg-slate-50 p-2">
-      <Card className="flex flex-col h-full flex-1">
+    <div className="flex-1 overflow-y-auto hide-scrollbar flex flex-col bg-slate-50 p-4 sm:p-6">
+      <Card className="flex flex-col h-full">
         {/* Messages */}
         <div className="flex-1 overflow-y-auto p-3 sm:p-6 space-y-3 min-h-0 hide-scrollbar">
           {messages.length === 0 ? (
