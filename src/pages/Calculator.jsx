c4343c4c920ca3e254123b8da601b1d49804034e
Shakil Ahmad <shakiluahmad@gmail.com>
2026-02-16 03:51:51 +0000

    File changes

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -311,7 +311,7 @@ export default function Calculator() {
       <div className="flex-1 overflow-y-auto hide-scrollbar">
         <div className="max-w-6xl mx-auto p-6 space-y-6">
           {/* Header Actions */}
-          <div className="flex justify-end gap-2 mb-4">
+          <div className="flex justify-end gap-2">
               {!isNewProject &&
               <div className="grid grid-cols-3 gap-2 w-fit">
                     <DropdownMenu>
