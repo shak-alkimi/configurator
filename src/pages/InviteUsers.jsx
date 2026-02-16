6ea858064ed88e4476fd22ed747398f0b18b350b
Shakil Ahmad <shakiluahmad@gmail.com>
2026-02-16 01:30:29 +0000

    File changes

--- src/pages/InviteUsers.jsx
+++ src/pages/InviteUsers.jsx
@@ -101,18 +101,15 @@ export default function InviteUsers() {
           <CardContent>
             <form onSubmit={handleSubmit} className="space-y-4">
               <div className="space-y-2">
-                <div className="relative">
-                  <Mail className="absolute left-3 top-2.5 h-4 w-4 text-slate-400" />
-                  <Input
-                    id="email"
-                    type="email"
-                    placeholder="user@email.com"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    className="pl-10 bg-white"
-                    required
-                  />
-                </div>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="user@email.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="bg-white"
+                  required
+                />
               </div>
 
               <div className="space-y-2">
