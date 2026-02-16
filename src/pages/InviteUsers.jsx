d54a6597f4caf86a296c5908c8f1081a49f11f6e
Shakil Ahmad <shakiluahmad@gmail.com>
2026-02-16 00:14:52 +0000

    File changes

--- src/pages/InviteUsers.jsx
+++ src/pages/InviteUsers.jsx
@@ -83,7 +83,6 @@ export default function InviteUsers() {
               src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/698fc81203f85a20f281d9dc/f2bc037c5_Screenshot2026-02-14160229.png" 
               alt="ALKIMI Logo"
               className="h-12"
-              style={{ filter: 'invert(1)' }}
             />
           </div>
         </div>
