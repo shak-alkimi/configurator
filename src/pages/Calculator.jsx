52a2a8977fe676eb946286c78458020976c1d269
Shakil Ahmad <shakiluahmad@gmail.com>
2026-02-16 23:00:39 +0000

    File changes

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -219,8 +219,8 @@ export default function Calculator() {
       </div>
 
       {/* Main Content */}
-      <div className="flex-1 overflow-y-auto">
-        <div className="max-w-7xl mx-auto p-6 space-y-6">
+      <div className="flex-1 overflow-y-auto p-4">
+        <div className="mx-auto space-y-6">
           {/* Header Actions */}
           <div className="flex items-center justify-between">
             <div>
