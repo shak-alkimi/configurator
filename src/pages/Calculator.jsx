11e7ef5cf988561a85be8c2f331eb301288a5618
Shakil Ahmad <shakiluahmad@gmail.com>
2026-02-16 04:04:20 +0000

    File changes

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -337,9 +337,9 @@ export default function Calculator() {
               }
           </div>
 
-          <div className="grid grid-cols-1 lg:grid-cols-5 gap-6">
+          <div className="grid grid-cols-1 lg:grid-cols-6 gap-6">
             {/* Left Column - Project Details */}
-            <div className="lg:col-span-4 space-y-6">
+            <div className="lg:col-span-5 space-y-6">
               <Card>
                 <CardHeader className="cursor-pointer" onClick={() => setDetailsExpanded(!detailsExpanded)}>
                   <div className="flex items-center justify-between">
