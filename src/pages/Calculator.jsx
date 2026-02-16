f0e13be16f4593bcc19d1b830e57ab31e1c442b0
Shakil Ahmad <shakiluahmad@gmail.com>
2026-02-16 23:18:18 +0000

    File changes

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -251,9 +251,9 @@ export default function Calculator() {
             </div>
           </div>
 
-          <div className="grid grid-cols-3 gap-6">
+          <div className="grid grid-cols-4 gap-6">
             {/* Left Column - Project Details */}
-            <div className="col-span-2 space-y-6">
+            <div className="col-span-3 space-y-6">
               <Card>
                 <CardHeader>
                   <CardTitle>Project Details</CardTitle>
