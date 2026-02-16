5f2a3cef35e9f22a4095c52ac55a2e9d2ff8185e
Shakil Ahmad <shakiluahmad@gmail.com>
2026-02-16 22:09:08 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,25 +2,18 @@ export default function Layout({ children, currentPageName }) {
   return (
     <div>
       <style>{`
-        [class*="bg-primary"],
-        [class*="bg-slate"],
-        button {
-          --button-color: #950B10;
+        :root {
+          --primary: 149 11 16;
         }
         
-        button,
-        [role="button"] {
-          background-color: #950B10;
-          color: white;
+        button[class*="default"],
+        .bg-primary {
+          background-color: #950B10 !important;
+          color: white !important;
         }
         
-        button:hover,
-        [role="button"]:hover {
-          background-color: #7a0809;
-        }
-        
-        button:disabled {
-          background-color: #cccccc;
+        button[class*="default"]:hover {
+          background-color: #7a0809 !important;
         }
       `}</style>
       {children}
