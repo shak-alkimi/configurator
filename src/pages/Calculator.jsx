da46f156961d744eb63b12d24a7eb919d5ac340d
Shakil Ahmad <shakiluahmad@gmail.com>
2026-02-16 03:31:06 +0000

    File changes

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -293,16 +293,10 @@ export default function Calculator() {
   };
 
   return (
-    <div className="h-screen flex bg-slate-50">
+    <div className="flex-1 overflow-y-auto hide-scrollbar flex bg-slate-50">
       {/* Sidebar - Projects List */}
-      <div className="w-80 border-r bg-white">
-        <div className="h-full flex flex-col">
-          
-
-
-
-
-
+      <div className="w-80 p-6 flex flex-col">
+        <Card className="flex-1 flex flex-col overflow-y-auto">
 
 
           <ProjectsList
