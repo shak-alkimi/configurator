57426008295852e354d52ce7e5b1130c3c79f246
Shakil Ahmad <shakiluahmad@gmail.com>
2026-02-17 01:11:57 +0000

    File changes

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -198,7 +198,7 @@ export default function Calculator() {
   };
 
   return (
-    <div className="h-screen flex gap-6 bg-white">
+    <div className="h-screen flex gap-4 bg-white">
       {/* Sidebar - Projects List */}
       <div className="w-80 px-6 py-4 flex flex-col">
         <Card className="h-full flex flex-col">
