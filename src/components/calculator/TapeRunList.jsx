6517cdd525debb95c6ad89627b1ad273736e3cbd
Shakil Ahmad <shakiluahmad@gmail.com>
2026-02-19 01:40:55 +0000

    File changes

--- src/components/calculator/TapeRunList.jsx
+++ src/components/calculator/TapeRunList.jsx
@@ -166,7 +166,7 @@ export default function TapeRunList({ runs, onAdd, onUpdate, onDelete, onReorder
                 className="h-9"
               />
             </div>
-            <div className="col-span-3 space-y-1.5">
+            <div className="col-span-2 space-y-1.5">
               <Label className="text-xs">Housing</Label>
               <Select
                 value={newRun.channel_type}
