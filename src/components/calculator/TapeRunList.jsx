b0db53320cb422a6bd08c24a88d7111ffac6cc75
Shakil Ahmad <shakiluahmad@gmail.com>
2026-02-21 20:20:12 +0000

    File changes

--- src/components/calculator/TapeRunList.jsx
+++ src/components/calculator/TapeRunList.jsx
@@ -195,8 +195,8 @@ export default function TapeRunList({ runs, onAdd, onUpdate, onDelete, onReorder
                 </SelectContent>
               </Select>
             </div>
-            <div className="col-span-1">
-              <Button onClick={handleAdd} size="sm" className="h-9 w-full" disabled={!isFormValid()}>
+            <div className="col-span-1 flex items-end">
+              <Button onClick={handleAdd} size="sm" className="h-9 w-10" disabled={!isFormValid()}>
                 <Plus className="h-4 w-4" />
               </Button>
             </div>
