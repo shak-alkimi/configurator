76e5dcc7bbf5ef15b6642123be6a1ef52d5ef8d0
Shakil Ahmad <shakiluahmad@gmail.com>
2026-02-21 20:35:15 +0000

    File changes

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -349,29 +349,44 @@ export default function Calculator() {
                   <>
                     {!isNewProject && (
                       <>
-                        <DropdownMenu>
-                          <DropdownMenuTrigger asChild>
-                            <Button variant="outline" size="icon" className="hidden sm:flex">
-                              <Download className="h-4 w-4" />
-                            </Button>
-                          </DropdownMenuTrigger>
-                          <DropdownMenuContent>
-                            <DropdownMenuItem onClick={handleExportCSV}>
-                              <FileDown className="h-4 w-4 mr-2" />
-                              Export as CSV
-                            </DropdownMenuItem>
-                            <DropdownMenuItem onClick={handleExportPDF}>
-                              <FileDown className="h-4 w-4 mr-2" />
-                              Export as PDF
-                            </DropdownMenuItem>
-                          </DropdownMenuContent>
-                        </DropdownMenu>
-                        <Button variant="outline" size="icon" onClick={handleSpecs} className="hidden sm:flex">
-                          <FileText className="h-4 w-4" />
-                        </Button>
-                        <Button variant="outline" size="icon" onClick={handleDeleteProject}>
-                          <Trash2 className="h-4 w-4" />
-                        </Button>
+                        <Tooltip>
+                           <TooltipTrigger asChild>
+                             <DropdownMenu>
+                               <DropdownMenuTrigger asChild>
+                                 <Button variant="outline" size="icon" className="hidden sm:flex">
+                                   <Download className="h-4 w-4" />
+                                 </Button>
+                               </DropdownMenuTrigger>
+                               <DropdownMenuContent>
+                                 <DropdownMenuItem onClick={handleExportCSV}>
+                                   <FileDown className="h-4 w-4 mr-2" />
+                                   Export as CSV
+                                 </DropdownMenuItem>
+                                 <DropdownMenuItem onClick={handleExportPDF}>
+                                   <FileDown className="h-4 w-4 mr-2" />
+                                   Export as PDF
+                                 </DropdownMenuItem>
+                               </DropdownMenuContent>
+                             </DropdownMenu>
+                           </TooltipTrigger>
+                           <TooltipContent>Export</TooltipContent>
+                         </Tooltip>
+                         <Tooltip>
+                           <TooltipTrigger asChild>
+                             <Button variant="outline" size="icon" onClick={handleSpecs} className="hidden sm:flex">
+                               <FileText className="h-4 w-4" />
+                             </Button>
+                           </TooltipTrigger>
+                           <TooltipContent>Specifications</TooltipContent>
+                         </Tooltip>
+                         <Tooltip>
+                           <TooltipTrigger asChild>
+                             <Button variant="outline" size="icon" onClick={handleDeleteProject}>
+                               <Trash2 className="h-4 w-4" />
+                             </Button>
+                           </TooltipTrigger>
+                           <TooltipContent>Delete Project</TooltipContent>
+                         </Tooltip>
                       </>
                     )}
                     <Button variant="outline" size="icon" onClick={() => toast.info('Submit functionality coming soon')}>
