82c2de7df10e6de8634fd09080c5217d57807b89
Shakil Ahmad <shakiluahmad@gmail.com>
2026-02-21 19:00:58 +0000

    File changes

--- src/components/calculator/ProjectsList.jsx
+++ src/components/calculator/ProjectsList.jsx
@@ -49,8 +49,8 @@ export default function ProjectsList({ projects, selectedId, onSelect, onNew, is
             >
               <CardContent className="p-3">
                 <div className="flex items-start gap-2 mb-2">
-                  <h3 className="font-semibold text-sm flex-1 break-words">{project.project_name}</h3>
-                  <Badge className={`${statusColors[project.status]} text-xs flex-shrink-0`}>
+                  <h3 className="font-semibold text-sm break-words">{project.project_name}</h3>
+                  <Badge className={`${statusColors[project.status]} text-xs flex-shrink-0 ml-auto`}>
                     {project.status}
                   </Badge>
                 </div>
